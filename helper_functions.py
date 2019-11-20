a48ca3c6a355e857a49f3308fcd9ca203189f4de
justasmach <justasmach@users.noreply.github.com>
2019-11-20 11:57:41 +0200

    finished scripts

--- helper_functions.py
+++ helper_functions.py
@@ -1,18 +1,14 @@
 ###### import everything upwards from home dir
 from __future__ import absolute_import
-import psycopg2
-import psycopg2.extras
+import psycopg2, argparse, six, re, datetime, json, yaml, sys, io, os, xlrd
 from configparser import ConfigParser
-import argparse
-import six
-import sys
+from os import path
+import psycopg2.extras
 import pandas as pd
-import re
-import datetime
 import numpy as np
-import json
-import yaml
-from xlrd import XLRDError
+import google.ads.google_ads.client
+from googleapiclient.discovery import build
+from googleapiclient import http
 from facebook_business.api import FacebookAdsApi
 from facebook_business.api import FacebookRequestError
 from facebook_business.adobjects.user import User
@@ -20,16 +16,14 @@ from facebook_business.adobjects.adaccount import AdAccount
 from facebook_business.adobjects.campaign import Campaign
 from facebook_business.adobjects.adset import AdSet
 from facebookads.adobjects.adsinsights import AdsInsights
-import threading
 from ast import literal_eval
 from datetime import datetime, timedelta
-import io
-import google.ads.google_ads.client
-from googleapiclient.discovery import build
-from googleapiclient import http
 from oauth2client.service_account import ServiceAccountCredentials
 
+# ---------------- DATABASE FUNCTIONS ----------------
 
+
+# get all the db configuration from file
 def db_config(filename, section):
     # create a parser
     parser = ConfigParser()
@@ -46,10 +40,45 @@ def db_config(filename, section):
         raise Exception('Section {0} not found in the {1} file'.format(section, filename)) 
     return db
 
-def create_table(tab_cr_str, conn, cur):
+# initiate db connection
+def init_conn():
+    conn = None
+    base, tail = os.path.split(os.getcwd())
+    filename = path.join(base, "database.ini")
+    params = db_config(filename, 'postgresql')
+    conn = psycopg2.connect(**params)
+    return conn
+
+# close db connection
+def end_conn(conn):
+    conn.close()
+
+# initiate db cursor
+def init_cur(conn):
+    cur = conn.cursor()
+    return cur  
+
+# close cursor
+def end_cur(cur):
+    cur.close()
+
+# create table using dynamic str
+def create_table(conn, cur, tab_cr_str):
     cur.execute(tab_cr_str)
     conn.commit()
     
+# drop table if defined by user at start of execution
+def drop_table(conn, cur, t_name, do_drop):
+    if do_drop:
+        cur.execute(f"DROP TABLE IF EXISTS {t_name};")
+    conn.commit()
+
+# using upsert_str to do a batch commit. better performace than row by row 
+def upsert(conn, cur, upsert_q, df_res_tuple, page_size):
+    psycopg2.extras.execute_values(cur, upsert_q, df_res_tuple)
+    conn.commit()
+    
+# dynamic table creation str for create_table function
 def tab_creation_str(t_name, pk_name, pk_lst, dtype_dict):
     result = ''
     columns = ''
@@ -58,46 +87,47 @@ def tab_creation_str(t_name, pk_name, pk_lst, dtype_dict):
     query_str = ("CREATE TABLE IF NOT EXISTS " + t_name + " " + 
                 "(" + columns + "CONSTRAINT " 
                + pk_name + " PRIMARY KEY (" + ', '.join(pk_lst) + "));")
-    return query_str, dtype_dict
+    return query_str
 
-def init_conn():
-    conn = None
-    params = db_config('database.ini', 'postgresql')
-    conn = psycopg2.connect(**params)
-    return conn
+# add column if not exists, used when creating the table
+# and when making multiple calls if additional columns are received
+def add_missing_cl(conn, cur, t_name, str_full):
+    if str_full != None:
+        cur.execute(f"ALTER TABLE {t_name} {str_full};")
+    conn.commit()
 
+# get present columns in the table if table already exists
+def get_db_cols(cur, t_name):
+    exists = cur.execute(f"SELECT to_regclass('{t_name}');")
+    column_names = []
+    if exists != None:
+        cur.execute(f"SELECT * FROM {t_name} WHERE False")
+        column_names = [desc[0] for desc in cur.description]
+    return column_names 
+
+# ---------------- REGEX ----------------
+
+
+# use regex to extract PLN from a single string
+def pln_no_reg(campaign_name):
+    return str(re.findall('(PLN?[\-]\d{1,4}?[\-]\d{1,4})', campaign_name))[2:-2]
+
+# use regex to extract only the campaign name from a single string
+def name_cl_reg(campaign_name):
+    pln = str(re.findall('(PLN?[\-]\d{1,4}?[\-]\d{1,4})', campaign_name))[2:-2]
+    name_cl = campaign_name.replace(pln, '')
+    name_cl = str(re.findall('^[^a-zA-Z\d]*(.*)', name_cl))[2:-2]
+    return name_cl
+
+# ---------------- STRING OPERATIONS ----------------
+
+
+# relpace numpy nan values with generic python nulls
 def rplc_nan(df_response):
     df_response = df_response.replace({pd.np.nan: None})
     return df_response
 
-def end_conn(conn):
-    conn.close()
-    
-def init_cur(conn):
-    cur = conn.cursor()
-    return cur
-    
-def add_column(t_name, db_dim, col_dtype, cur):
-    cur.execute(f"ALTER TABLE {t_name} "
-                f"ADD COLUMN IF NOT EXISTS {db_dim} {col_dtype};")
-
-def drop_table(t_name, do_drop, cur):
-    if do_drop:
-        cur.execute(f"DROP TABLE IF EXISTS {t_name};")
-    
-def add_ts(dataframe):
-    dataframe.insert(0, 'creation_ts', datetime.now())
-    dataframe.insert(0, 'last_updated_ts', datetime.now())
-    return dataframe 
-
-def get_pln_no(df_response, src_col_name, is_in_df):
-    if is_in_df:
-        df_response.insert(0, 'pln_no', df_response[src_col_name].apply(pln_no_reg))
-    return df_response
-    
-def pln_no_reg(campaign_name):
-    return str(re.findall('(PLN?[\-]\d{1,4}?[\-]\d{1,4})', campaign_name))[2:-2]
-
+# rename column headers if any unsupported chars are found, due to db limitations
 def rename_col(col_name):
     if '.' in col_name:
         return col_name.replace('.', '_')
@@ -107,32 +137,8 @@ def rename_col(col_name):
         return ('_' + col_name)
     else:
         return col_name
-    
-def get_types(df_response):   
-    col_names = df_response.head(0).to_dict()
-    col_dtypes = {}
-    dtype = ''
-    for key in col_names:
-            for i, row in df_response.iterrows():
-                try:
-                    
-                    dtype = ''
-                    if re.match('\d{4}\-\d{2}\-\d{2}\s+\d{2}\:\d{2}\:\d{2}\.\d+', str(row[key])):
-                        dtype = 'TIMESTAMP'
-                        break
-                    elif (re.match('\d{4}\-\d{2}\-\d{2}', str(row[key])) and 'keyword' not in str(key).lower()) or 'ga_date' in str(key).lower():
-                        dtype = 'DATE'
-                        break
-                    elif 'float' in str(type(literal_eval(row[key]))):
-                        dtype = 'REAL'
-                        break
-                    else:
-                        dtype = 'BIGINT'
-                except (ValueError, SyntaxError):
-                    dtype = 'VARCHAR'
-            col_dtypes.update({key : dtype})
-    return col_dtypes
-        
+
+# dynamic update or insert string, updates everything except for creation_ts
 def upsert_str(dims, t_name, pk):
     upsert_q = (f"INSERT INTO {t_name} ({dims}) "
                         "VALUES %s "
@@ -142,30 +148,104 @@ def upsert_str(dims, t_name, pk):
                                 f"SET creation_ts = EXCLUDED.creation_ts; ")
     return str(upsert_q)
 
-def upsert(conn, cur, upsert_q, df_res_tuple, page_size):
-    psycopg2.extras.execute_values(cur, upsert_q, df_res_tuple)
-    conn.commit()
-    cur.close()
+# dynamic missing column string, adds columns if missing between API calls
+def add_missing_cl_str(dtype_dict, db_cols):
+    str_full = ''
+    if len(dtype_dict) != len(db_cols):
+        str_part = 'ADD COLUMN IF NOT EXISTS '
+        for key in dtype_dict:
+            str_full = str_full + str_part + key + ' ' + dtype_dict[key] + ', '
+        str_full = str_full[:-2]     
+    return str_full
 
+# ---------------- DATAFRAME OPERATIONS ----------------
+
+
+# apply pln_no_reg and insert new column to dataframe with the clean PLN
+def get_pln_no(df_response, src_col_name, is_in_df):
+    if is_in_df:
+        df_response.insert(0, 'pln_no', df_response[src_col_name].apply(pln_no_reg))
+    return df_response
+
+# apply name_cl_reg and insert new column to dataframe with the clean campaign name 
+def get_name_col(df_response, src_col_name, is_in_df):
+    if is_in_df:
+        df_response.insert(0, 'campaing_name_noPLN', df_response[src_col_name].apply(name_cl_reg))
+    return df_response
+
+# add timestamps for every row in the dataframe
+def add_ts(dataframe):
+    dataframe.insert(0, 'creation_ts', datetime.now())
+    dataframe.insert(0, 'last_updated_ts', datetime.now())
+    return dataframe 
+
+# get data types for every column
+def get_types(df_response):   
+    col_names = df_response.head(0).to_dict()
+    col_dtypes = {}
+    for key in col_names:
+        is_ts = False
+        is_date = False
+        is_real = False
+        is_int = False
+        dtype = ''
+        # iterates through every column and row, to assign proper data type (includes exceptions)
+        for i, row in df_response.iterrows():
+            try:
+                if row[key] is None:
+                    pass
+                elif re.match('\d{4}\-\d{2}\-\d{2}\s+\d{2}\:\d{2}\:\d{2}\.\d+', str(row[key])):
+                    is_ts = True
+                    break
+                elif (re.match('\d{4}\-\d{2}\-\d{2}', str(row[key])) and 'keyword' not in str(key).lower()) or 'ga_date' in str(key).lower():
+                    is_date = True
+                    break
+                elif 'float' in str(type(literal_eval(row[key]))) or str(key).lower() == 'value' or str(key).lower() == 'spend' or 'd_view' in str(key).lower() or 'd_click' in str(key).lower():
+                    is_real = True
+                    break
+                elif 'int' in str(type(literal_eval(row[key]))):
+                    is_int = True
+            except (ValueError, SyntaxError):
+                pass
+        if is_ts:
+            dtype = 'TIMESTAMP'
+        elif is_date:
+            dtype = 'DATE'
+        elif is_real:
+            dtype = 'REAL'
+        elif is_int:
+            dtype = 'BIGINT'
+        else:
+            dtype = 'VARCHAR'
+        col_dtypes.update({key : dtype})
+    return col_dtypes
+
+# ---------------- MAIN ----------------
+
+# main method which calls all functions in sequence
 def postgre_write_main(df_response, t_name, pk_name, pk_lst, do_drop, page_size, src_col_name, is_pln_df):
     try:
         df_response = df_response.rename(columns=rename_col)
         df_response = get_pln_no(df_response, src_col_name, is_pln_df)
+        df_response = get_name_col(df_response, src_col_name, is_pln_df)
         df_response = rplc_nan(df_response)
         df_response = add_ts(df_response)
         dtype_dict = get_types(df_response)
         conn = init_conn()
         cur = init_cur(conn)
-        drop_table(t_name, do_drop, cur)
-        tab_creation = tab_creation_str(t_name, pk_name, pk_lst, dtype_dict)
-        tab_cr_str = tab_creation[0]
-        tab_types = tab_creation[1]
-        create_table(tab_cr_str, conn, cur)
+        db_cols = get_db_cols(cur, t_name)
+        drop_table(conn, cur, t_name, do_drop)
+        tab_cr_str = tab_creation_str(t_name, pk_name, pk_lst, dtype_dict)
+        create_table(conn, cur, tab_cr_str)
 
+        missing_cl_str = add_missing_cl_str(dtype_dict, db_cols)
+        add_missing_cl(conn, cur, t_name, missing_cl_str)
+        
         df_res_tuple = list(df_response.itertuples(index=False, name=None))
         upsert_q = upsert_str((','.join(dtype_dict.keys())), t_name, (','.join(pk_lst)))
         upsert(conn, cur, upsert_q, df_res_tuple, page_size)    
-
+        
+        end_cur(cur)
         end_conn(conn)
     except (Exception, psycopg2.DatabaseError) as error:
         print('Database error')
@@ -176,20 +256,3 @@ def postgre_write_main(df_response, t_name, pk_name, pk_lst, do_drop, page_size,
             conn.close()
     print('Database connection closed.')
     print('')
-
-def period_split(def_period, def_intv):
-
-    start = datetime.strptime(list(def_period.values())[0],"%Y-%m-%d")
-    end = datetime.strptime(list(def_period.values())[1],"%Y-%m-%d")
-    diff = (end  - start ) / def_intv
-    period_lst = []
-    curr = start
-    period_lst.append(start.strftime("%Y-%m-%d"))
-    while True:
-        curr = curr + timedelta(days = def_intv)
-        if curr >= end:
-            break
-        else:
-            period_lst.append(curr.strftime("%Y-%m-%d"))
-    period_lst.append(end.strftime("%Y-%m-%d"))
-    return period_lst
